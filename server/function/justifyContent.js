bd879d8d987b4c1bf1a2287ebdf51f074be53a71
YoannP-08 <yoann.pons08@gmail.com>
2021-02-19 17:58:34 +0100

    [MOD] justify content done

--- server/function/justifyContent.js
+++ server/function/justifyContent.js
@@ -1,31 +1,29 @@
-
 function justifyContent(text) {
     const nbrRound = Math.round(text.length / 80);
     let newText = '';
 
+    // Removing extra whitespace
+    text = text.replace(/\s\s+/g, ' ');
+
     for (let i = 0; i < nbrRound; i++) {
-        // Verifying if lastline to not add unnecessary new line
+        // Verifying if it's lastline to not add unnecessary new line
         if ((i + 1) === nbrRound) {
             let tmpTxt = text.slice(0, 80);
-            newText += tmpTxt;
+            newText += tmpTxt.trim();
         } else {
             let tmpTxt = text.slice(0, 80);
             text = text.slice(80);
-            newText += tmpTxt + '\n';
+
+            if (tmpTxt.startsWith(' ') || tmpTxt.endsWith(' ')) {            
+                newText += tmpTxt.trim() + ' ' + text.slice(0, 1) + '\n';
+                text = text.slice(1)
+            } else {
+                newText += tmpTxt + '\n';
+            };
         };
     };
 
-    alignContent(newText)
-
     return newText;
-};
-
-// Align Content if line starts or ends with
-// a whitespace
-function alignContent (text) {
-    let arrAllLines = text.split(/\n/);
-
-    console.log(arrAllLines)
 };
 
 module.exports = justifyContent;
